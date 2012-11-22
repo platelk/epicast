c1ac43d04f366bb107c9ede1d9b60187a1a77a89
jspdown <harold.ozouf@epitech.eu>
2012-11-22 12:57:36 +0100

    finalisation du delete.php, fixe des bugs des error dans toutes les pages, NEW DB

--- php/delete.php
+++ php/delete.php
@@ -4,13 +4,9 @@ header("Content-Type: application/json");
 if (isset($_POST['id']) && !empty($_POST['id']) &&
     isset($_POST['type']) && !empty($_POST['type']))
   {
-    if ($_POST['type'] == "videos" || $_POST['type'] == "folders" ||
-	$_POST['type'] == "channels")
-      {
-	echo "CONNECT...\n";
-	echo "LET'S RECUSIVE...\n";
-	delete_folder($_POST['type'], $_POST['id']);
-      }
+    if ($_POST['type'] == "video" || $_POST['type'] == "folder" ||
+	$_POST['type'] == "channel" || $_POST['type'] == "user")
+      delete_folder($_POST['type'], $_POST['id']);
     else
       echo "{\"Error\": \"Bad request\"}";
   }
@@ -21,35 +17,41 @@ else
 function delete_folder($type, $id)
 {
   $db = new PDO('mysql:host=localhost;dbname=test', 'root', '');
-  echo "=====> TYPE: " . $type . " ID: " . $id . "\n";
   if ($type == "folders")
     {
-      echo $id . " |DELETE: FOLDERS\n";
       $prepared = $db->prepare('CALL get_folder_content(:id)');
       $prepared->execute(array('id' => $id));
-      $section = array("folders", "channels", "videos");
+      $section = array("folder", "channel");
       foreach($section as $i)
 	{
 	  while($line = $prepared->fetch(PDO::FETCH_ASSOC))
-	    {
-	      echo $id . " |" . $i . " |line: " . $line['id'] . " " . $line['name'] . $i . "\n";
-	      delete_folder($i, $line['id']);
-	    }
+	    delete_folder($i, $line['id']);
 	  $prepared->nextRowset();
 	}
-      //      $prepared = $db->prepare('CALL delete_folder(:id)');
-      //$prepared->execute(array('id' => $id));
+      $prepared = $db->prepare('CALL delete_folder(:id)'); */
+      $prepared->execute(array('id' => $id)); */
    }
-  else if ($type == "channels")
+  else if ($type == "channel")
     {
-      echo $id . " |DELETE: CHANNELS\n";
-      //$prepared = $db->prepare('CALL delete_channel(:id)');
-      //$prepared->execute(array('id' => $id));
+      $prepared = $db->prepare('CALL delete_channel(:id)');
+      $prepared->execute(array('id' => $id));
     }
-  else if ($type == "videos")
+  else if ($type == "video")
     {
-      echo $id . " |DELETE: CHANNELS\n";
+      $prepared = $db->prepare('CALL delete_video(:id)');
+      $prepared->execute(array('id' => $id));
     }
+  else if ($type == "user")
+    {
+      $prepared = $db->prepare('CALL get_user_informations(:id);');
+      $prepared->execute(array('id' => $id));
+      $data = $prepared->fetch(PDO::FETCH_ASSOC);
+      delete_folder("folder", $data['folders_id']);
+      $prepared = $db->prepare('CALL delete_user(:id);');
+      $prepared->execute(array('id' => $id));
+    }
+  else
+    echo "{\"Error\": \"Bad request\"}";
   $db = null;
 }
 ?>
