dde9fdd7d6749cd86f81d5e4be0d0156a42e40ac
jspdown <harold.ozouf@epitech.eu>
2012-11-23 11:08:43 +0100

    add add_video.php mod

--- php/add_video.php
+++ php/add_video.php
@@ -5,16 +5,19 @@ if(isset($_SESSION['id']) &&
    isset($_POST['name']) && !empty($_POST['name']) &&
    isset($_POST['description']) && !empty($_POST['description']) &&
    isset($_POST['image']) && !empty($_POST['image']) &&
-   isset($_POST['video']) && !empty($_POST['video']) &&
-   isset($_POST['live']) && !empty($_POST['live']))
+   isset($_POST['video']) && !empty($_POST['video']))
   {
+    if (isset($_POST['live']) && !empty($_POST['live']))
+      $live = $_POST['live'];
+    else
+      $live = 0;
     $db = new PDO('mysql:host=localhost;dbname=test', 'root', '');
     $prepared = $db->prepare('CALL add_video(:name, :description, :image, :video, :live, :user_id)');
     $prepared->execute(array('name' => $_POST['name'],
 			     'description' => $_POST['description'],
 			     'image' => $_POST['image'],
 			     'video' => $_POST['video'],
-			     'live' => $_POST['live'],
+			     'live' => $live,
 			     'user_id' => $_SESSION['id']));
     $user_id = $_SESSION['id'];
     $db = null;
