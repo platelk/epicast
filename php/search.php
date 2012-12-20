37e5a6a21a7ff6a23247d04968e908cd11eb4c41
louaze_j <jean-baptiste.louazel@epitech.eu>
2012-12-20 16:27:53 +0100

    plop

--- php/search.php
+++ php/search.php
@@ -2,17 +2,24 @@
 header("Content-Type: application/json");
 if (isset($_POST['req']) && !empty($_POST['req']))
   {
+    $ar = array(
+		'SELECT username AS users FROM users WHERE username LIKE "'.$_POST['req'].'%"',
+		'SELECT name AS videos FROM videos WHERE name LIKE "'.$_POST['req'].'%"',
+		);
     $bdd = new PDO('mysql:host=localhost;dbname=test', 'root', '');
-    $answer = $bdd->prepare('CALL dynamic_search(:req)');
-    $answer->execute(array('req' => $_POST['req']));
-    $i = 0;
     echo '[';
-    while ($data = $answer->fetch(PDO::FETCH_ASSOC))
+    for ($p = 0; $p != 1; $p++)
       {
-      	if ($i != 0)
-      	  echo ",";
-	echo json_encode($data);
-      	$i++;
+	$answer = $bdd->prepare($ar[$p]);
+	$answer->execute(array('req' => $_POST['req']));
+	$i = 0;
+	while ($data = $answer->fetch(PDO::FETCH_ASSOC))
+	  {
+	    if ($i != 0)
+	      echo ",";
+	    echo json_encode($data);
+	    $i++;
+	  }
       }
     echo ']';
     $bdd = null;
